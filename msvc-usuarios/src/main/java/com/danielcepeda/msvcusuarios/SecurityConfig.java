a08e80d1851bca580424b539ac2428424852faac
Daniel Esteban <decepeda@misena.edu.co>
2023-01-11 15:30:12 -0500

    se configura el resource server

--- msvc-usuarios/src/main/java/com/danielcepeda/msvcusuarios/SecurityConfig.java
+++ msvc-usuarios/src/main/java/com/danielcepeda/msvcusuarios/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.danielcepeda.msvcusuarios;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,12 +13,20 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeRequests().anyRequest().authenticated()
+        httpSecurity.authorizeRequests()
+                .antMatchers("/authorized").permitAll()
+                .antMatchers(HttpMethod.GET, "/", "/{id}")
+                .hasAnyAuthority("SCOPE_read", "SCOPE_write")
+                .antMatchers(HttpMethod.POST, "/").hasAuthority("SCOPE_write")
+                .antMatchers(HttpMethod.DELETE, "/{id}").hasAuthority("SCOPE_write")
+                .antMatchers(HttpMethod.DELETE, "/{id}").hasAuthority("SCOPE_write")
+                .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .oauth2Login(oauth2Login -> oauth2Login.loginPage("/oauth2/authorization/msvc-usuarios-client"))
-                .oauth2Client(Customizer.withDefaults());
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer().jwt();
         return httpSecurity.build();
     }
 }
